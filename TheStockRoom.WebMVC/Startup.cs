8804081c8ef763b1e5f871cb04a36b08ad23c307
Alecyancey <59926558+Alecyancey@users.noreply.github.com>
2020-03-30 22:16:14 -0400

    Add project files.

---
+++ TheStockRoom.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TheStockRoom.WebMVC.Startup))]
+namespace TheStockRoom.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
